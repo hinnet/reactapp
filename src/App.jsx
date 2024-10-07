8fc5b11ea5bef6b790c0bbaf3ab6dda98042d086
Netta <bhi084@myy.haaga-helia.fi>
2024-10-07 22:31:06 +0300

    tab-menu tehty

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,35 @@
+import React from 'react'
 import TodoList from './TodoList'
 import Container from '@mui/material/Container'
 import CssBaseline from '@mui/material/CssBaseline'
 import AppBar from '@mui/material/AppBar'
 import Toolbar from '@mui/material/Toolbar'
 import Typography from '@mui/material/Typography'
+import { Tab, Tabs } from '@mui/material'
 
 function App() {
 
+  const [value, setValue] = React.useState('1');
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue)
+  }
+
   return (
-    <Container maxWidth='l'>
+    <Container maxWidth='xl'>
       <CssBaseline />
-      <AppBar position="static">
-        <Toolbar>
-          <Typography variant="h3">
-            My Todos
+      <Tabs value={value} onChange={handleChange} aria-label="navi">
+        <Tab label="Home" value="1" />
+        <Tab label="Todos" value="2" />
+      </Tabs>
+      {value === '1' && (
+        <div>
+          <Typography variant='h3' >
+            Tervetuloa MyTodos-sovellukseen!
           </Typography>
-        </Toolbar>
-      </AppBar>
-      <TodoList />
+        </div>
+      )}
+      {value === '2' && <TodoList />}
     </Container>
   )
 }
